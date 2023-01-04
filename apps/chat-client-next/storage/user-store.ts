4fa96803a72ccb0ac72d6d32f3bceefc4970bbdb
Max Netterberg <max.netterberg@gmail.com>
2023-01-04 21:55:23 +0100

    added a deafult user if there is none :)

--- apps/chat-client-next/storage/user-store.ts
+++ apps/chat-client-next/storage/user-store.ts
@@ -4,10 +4,19 @@ export type UserStoreState = {
 };
 
 const key = "user-store";
+const defaultUser = {
+  username: "Default user",
+  hashedUsername:
+    "942b74ac58192ee4c82f03e67064c52bf6baa0dc88663e5f801d3d16daf70ff1",
+};
 
 export const UserStore = {
   get() {
-    return JSON.parse(localStorage.getItem(key) ?? "{}") as UserStoreState;
+    const json = localStorage.getItem(key);
+    if (!json) {
+      return defaultUser;
+    }
+    return JSON.parse(json) as UserStoreState;
   },
   set(state: UserStoreState) {
     localStorage.setItem(key, JSON.stringify(state));
