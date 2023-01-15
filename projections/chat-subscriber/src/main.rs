50ebbbba9ab6cd12779fe024b3904e37ea758851
Max Netterberg <max.netterberg@gmail.com>
2023-01-15 14:07:27 +0100

    No longer creating persistant connections when clients connect

--- projections/chat-subscriber/src/main.rs
+++ projections/chat-subscriber/src/main.rs
@@ -6,52 +6,27 @@ use std::{
     },
 };
 
-use actix::{Actor, AsyncContext, Handler, Message, StreamHandler};
+use actix::{spawn, Actor, AsyncContext, Handler, Message, StreamHandler};
 use actix_cors::Cors;
 use actix_web::{
-    http::{self},
+    http,
     middleware::Logger,
-    web::{self, Data},
-    App, Error, HttpRequest, HttpResponse, HttpServer,
+    web::{get, Data, Payload, Query},
+    App, Error as ActixError, HttpRequest, HttpResponse, HttpServer,
 };
-use actix_web_actors::ws::{self};
+use actix_web_actors::ws;
 use events::ChatMessageSentEvent;
-use eventstore::{
-    Client, DeletePersistentSubscriptionOptions, PersistentSubscription,
-    PersistentSubscriptionOptions, StreamPosition,
-};
-use eyre::Result;
-use log::info;
+use eventstore::{Client, PersistentSubscriptionOptions, StreamPosition};
+use eyre::{Error, Result};
+use log::{error, info, warn};
 use serde::Deserialize;
 use serde_json::json;
-use uuid::Uuid;
+use tokio::sync::broadcast::Sender;
 
-async fn create_sub(client: &Client, consumer_grp: &str) -> Result<PersistentSubscription> {
-    info!("Creating persistent subscription with id {}", consumer_grp);
-    let options = PersistentSubscriptionOptions::default().start_from(StreamPosition::End);
-    client
-        .create_persistent_subscription("chat-stream", consumer_grp, &options)
-        .await?;
-    client
-        .subscribe_to_persistent_subscription("chat-stream", consumer_grp, &Default::default())
-        .await
-        .map_err(eyre::Error::from)
-}
-
-async fn delete_sub(client: &Client, consumer_grp: &str) -> Result<()> {
-    info!("Deleting subscription with id {}", consumer_grp);
-    let options = DeletePersistentSubscriptionOptions::default();
-    client
-        .delete_persistent_subscription("chat-stream", consumer_grp, &options)
-        .await
-        .map_err(eyre::Error::from)
-}
-
-/// Define HTTP actor
 struct ChatServer {
     client_count: Arc<AtomicUsize>,
     chat_id: String,
-    consumer_grp_id: String,
+    tx: Sender<ChatMessageSentEvent>,
 }
 
 impl Actor for ChatServer {
@@ -60,14 +35,12 @@ impl Actor for ChatServer {
 
 #[derive(Message)]
 #[rtype(result = "()")]
-pub struct Event {
-    body: String,
-}
+struct Event(String);
 impl Handler<Event> for ChatServer {
     type Result = ();
 
     fn handle(&mut self, msg: Event, ctx: &mut Self::Context) {
-        ctx.text(msg.body);
+        ctx.text(msg.0);
     }
 }
 
@@ -80,62 +53,40 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ChatServer {
         );
         let recipient = ctx.address();
         let chat_id = self.chat_id.clone();
-        let group_id = self.consumer_grp_id.clone();
-        let client = create_eventstore_client().expect("Unable to create eventstore client");
+        let mut rx = self.tx.subscribe();
         let fut = async move {
-            let mut sub = match create_sub(&client, &group_id).await {
-                Ok(sub) => sub,
-                Err(_) => return,
-            };
-
             loop {
-                let event = sub.next().await.map(|it| it.event).ok().flatten();
-                let event = match event {
-                    Some(it) => it,
-                    None => {
-                        log::error!("None was received in recorded event, breaking loop");
+                let event = match rx.recv().await {
+                    Ok(e) => e,
+                    Err(err) => {
+                        error!("Unable to receive event from channel. {}", err.to_string());
                         break;
                     }
                 };
-                if let Ok(event) = event.as_json::<ChatMessageSentEvent>() {
-                    if event.chat_id == chat_id {
-                        let json_string = json!({
-                            "message": event.message,
-                            "sent_by": event.user_id,
-                            "message_id": event.message_id,
-                        })
-                        .to_string();
-                        let dto = Event { body: json_string };
-                        recipient.do_send(dto);
-                    }
-                }
-                sub.ack_ids(vec![event.id])
-                    .await
-                    .expect("unable to ack event");
+
+                if event.chat_id != chat_id {
+                    continue;
+                };
+
+                let json_string = json!({
+                    "message": event.message,
+                    "sent_by": event.user_id,
+                    "message_id": event.message_id,
+                })
+                .to_string();
+                recipient.do_send(Event(json_string))
             }
         };
         let fut = actix::fut::wrap_future::<_, Self>(fut);
-        // Since the execution of the future is automatically closed when the
-        // StreamHandler is finished there is no need to manually drop the connection to
-        // Eventstore
         ctx.spawn(fut);
     }
 
-    fn finished(&mut self, ctx: &mut Self::Context) {
+    fn finished(&mut self, _: &mut Self::Context) {
         self.client_count.fetch_sub(1, Ordering::SeqCst);
         info!(
             "Client disconnected. #clients {}",
             self.client_count.load(Ordering::SeqCst)
         );
-        let group_id = self.consumer_grp_id.clone();
-        let client = create_eventstore_client().expect("Unable to create eventstore client");
-        let fut = async move {
-            delete_sub(&client, &group_id)
-                .await
-                .expect("Unable to delete subscription")
-        };
-        let fut = actix::fut::wrap_future(fut);
-        ctx.spawn(fut);
     }
 
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
@@ -153,26 +104,62 @@ struct WsQuery {
 
 async fn index(
     req: HttpRequest,
-    stream: web::Payload,
+    stream: Payload,
     client_count: Data<Arc<AtomicUsize>>,
-    query: web::Query<WsQuery>,
-) -> Result<HttpResponse, Error> {
-    let consumer_grp_id = Uuid::new_v4().to_string();
+    query: Query<WsQuery>,
+    tx: Data<Sender<ChatMessageSentEvent>>,
+) -> Result<HttpResponse, ActixError> {
     ws::start(
         ChatServer {
             client_count: client_count.as_ref().clone(),
             chat_id: query.chat_id.clone(),
-            consumer_grp_id,
+            tx: tx.as_ref().clone(),
         },
         &req,
         stream,
     )
 }
 
+async fn create_sub(client: &Client, consumer_grp: &str) -> Result<()> {
+    info!("Creating persistent subscription with id {}", consumer_grp);
+    let options = PersistentSubscriptionOptions::default().start_from(StreamPosition::End);
+    client
+        .create_persistent_subscription("chat-stream", consumer_grp, &options)
+        .await
+        .map_err(eyre::Error::from)
+}
+
 fn create_eventstore_client() -> Result<Client> {
     let uri = env::var("EVENTSTORE_URI")?;
     let settings = uri.parse()?;
     Client::new(settings).map_err(eyre::Error::from)
+}
+
+async fn bootstrap_es(tx: Sender<ChatMessageSentEvent>) -> Result<()> {
+    let client = create_eventstore_client().expect("Unable to create es client");
+    let grp_id = env::var("GROUP_ID")?;
+    match create_sub(&client, &grp_id).await {
+        Ok(_) => info!("Created group {}", &grp_id),
+        Err(err) => warn!("Err creating group {}", err.to_string()),
+    };
+
+    let mut sub = client
+        .subscribe_to_persistent_subscription("chat-stream", grp_id, &Default::default())
+        .await?;
+
+    loop {
+        let event = sub
+            .next()
+            .await
+            .map(|it| it.event)?
+            .ok_or(Error::msg("Unable to get next event"))?;
+
+        if let Ok(event) = event.as_json::<ChatMessageSentEvent>() {
+            tx.send(event)?;
+        }
+
+        sub.ack_ids(vec![event.id]).await?;
+    }
 }
 
 fn create_cors() -> Cors {
@@ -192,6 +179,8 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let client_count = Arc::new(AtomicUsize::new(0));
     let port = 8082;
+    let (tx, _) = tokio::sync::broadcast::channel::<ChatMessageSentEvent>(100);
+    spawn(bootstrap_es(tx.clone()));
     info!("Started server at http://localhost:{}", port);
     HttpServer::new(move || {
         let cors = create_cors();
@@ -199,7 +188,8 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .wrap(cors)
             .app_data(Data::new(client_count.clone()))
-            .route("/ws/", web::get().to(index))
+            .app_data(Data::new(tx.clone()))
+            .route("/ws/", get().to(index))
     })
     .workers(4)
     .bind(("0.0.0.0", port))?
